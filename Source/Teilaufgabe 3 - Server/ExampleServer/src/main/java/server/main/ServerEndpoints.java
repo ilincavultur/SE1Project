43443d9495bfa1680ea4eb9c034744982c3e9e47
Ilinca Vultur <a11925311@unet.univie.ac.at>
2022-06-06 20:25:39 +0200

    get states 14/14 successful

--- Source/Teilaufgabe 3 - Server/ExampleServer/src/main/java/server/main/ServerEndpoints.java
+++ Source/Teilaufgabe 3 - Server/ExampleServer/src/main/java/server/main/ServerEndpoints.java
@@ -142,13 +142,13 @@ public class ServerEndpoints {
 			throw e;
 		}
 		
-		/*try {
+		try {
 			rules.forEach(rule -> rule.validateGameState(gameStateController.getGames(), new UniquePlayerIdentifier(halfMap.getUniquePlayerID()), gameID));	
 		} catch (NotEnoughPlayersException e) {
 			throw e;
-		}*/
+		}
 		
-		if (!gameStateController.bothPlayersRegistered(gameID)) {
+		if (gameStateController.bothPlayersRegistered(gameID) == false) {
 			gameStateController.getGames().get(gameID.getUniqueGameID()).setWinner(halfMap.getUniquePlayerID());
 			throw new NotEnoughPlayersException("Only one client has registered", "Client tried to send half Map but not both players were registered");
 		}
